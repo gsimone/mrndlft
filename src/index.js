b4b3863b4531ea61ee862c53c8038be00741cd37
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-11-17 12:56:44 +0100

    Removes imagesLoaded preload to avoid double fetching

--- src/index.js
+++ src/index.js
@@ -4,30 +4,22 @@ import NetflixSlider from "./netflix-slider";
 
 const container = document.body;
 const itemsWrapper = document.querySelector(".grid");
-const preloadImages = () => {
-  return new Promise((resolve, reject) => {
-    imagesLoaded(document.querySelectorAll("img"), resolve);
-  });
-};
 
-preloadImages().then(() => {
-  document.body.classList.remove("loading");
+document.body.classList.remove("loading");
 
-  const effect = new StretchEffect(container, itemsWrapper);
-  const slider = new NetflixSlider(document.querySelector(".grid"));
-  slider.init();
+const effect = new StretchEffect(container, itemsWrapper);
+const slider = new NetflixSlider(document.querySelector(".grid"));
+slider.init();
 
-  const titles = Array.from(document.querySelectorAll('.title-w .w-dyn-item'))
+const titles = Array.from(document.querySelectorAll('.title-w .w-dyn-item'))
 
+effect.isMobile = window.innerWidth < 800
+window.addEventListener("resize", () => {
   effect.isMobile = window.innerWidth < 800
-  window.addEventListener("resize", () => {
-    effect.isMobile = window.innerWidth < 800
-    console.log(effect)
-  });
-  
-  slider.onChange = (activeIndex) => {
-    titles.forEach((title, i) => {
-      title.classList.toggle("active", activeIndex === i)
-    })
-  }
 });
+
+slider.onChange = (activeIndex) => {
+  titles.forEach((title, i) => {
+    title.classList.toggle("active", activeIndex === i)
+  })
+}
